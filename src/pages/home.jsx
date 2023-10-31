b0d7cf75c6cea710c093f1f2285d0c0f6d49f57f
Joyonto Karmakar <joyonto1997@gmail.com>
2023-11-01 00:23:18 +0600

    layout initialize

---
+++ src/pages/home.jsx
@@ -0,0 +1,20 @@
+
+export const Home = () => {
+    return (
+        <div>
+            <div className="p-5">
+                <div className="grid grid-cols-4 gap-x-2 gap-y-2">
+                    <div className="divStyle h-[108px] col-span-2 row-span-2">1</div>
+                    <div className="divStyle h-[50px] col-span-1">2</div>
+                    <div className="divStyle h-[50px] col-span-1">3</div>
+                    <div className="divStyle h-[50px] col-span-1">4</div>
+                    <div className="divStyle h-[50px] col-span-1">5</div>
+                    <div className="divStyle h-[50px] col-span-1">6</div>
+                    <div className="divStyle h-[50px] col-span-1">7</div>
+                    <div className="divStyle h-[50px] col-span-1">8</div>
+                    <div className="divStyle h-[50px] col-span-1">9</div>
+                </div>
+            </div>
+        </div>
+    )
+}
